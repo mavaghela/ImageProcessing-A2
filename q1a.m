bb0dbeee862044d5ef8b287416a3cc545dd176a9
Mansi Vaghela <mvaghela@utmuser171-231.wireless.utoronto.ca>
2017-02-16 17:52:27 -0500

    computing difference of gaussians, no idea if this works

--- q1a.m
+++ q1a.m
@@ -17,7 +17,22 @@ for i = 1:octave
     imgPyramid(i, j) = imfilter(imgPyramid(i-1), gFilter, 'conv');
   end
 end
-  
+
+imgDiff = [];
+
+k = 2^(1/scale);
+
+for i = 1:octave
+  for j = 1:scale
+    # p = (sigma, sigma*k,..., sigma*k^scale -1)
+    gaussian1 = fspecial('Gaussian', 10, ((k*(j-1))*sigma));  #G(x,y,p)
+    gaussian2 = fspecial('Gaussian', 10, k*((k*(j-1))*sigma)); #G(x,y,kp)
+    dog = gaussian2 - gaussian1;
+    dogFilterImage = conv2(imgPyramid(i, j), dog, 'same');
+    imgDiff(i, j) = dogFilterImage;
+  end
+end
+    
 ## Need to compute difference of gaussians now 
  
 ## difference of gaussians
