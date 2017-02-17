5200f19fd6e6730e1bf79d92cb1b63d957d9b86a
Mansi Vaghela <mvaghela@utmuser171-231.wireless.utoronto.ca>
2017-02-16 19:10:59 -0500

    fixed code so its actually taking difference of two guassian images

--- q1a.m
+++ q1a.m
@@ -7,14 +7,18 @@ scale = 5;
 
 ## Gaussian image pyramid
 
-gFilter = imgaussfilt(img,sigma);
 imgPyramid = [];
-
-imgPyramid(1, 1) = imfilter(img, gFilter, 'conv');
+I = img;
 
 for i = 1:octave
-  for j = 2:scale
-    imgPyramid(i, j) = imfilter(imgPyramid(i-1), gFilter, 'conv');
+  if( i > 1)
+    [M, N] = size(I);
+    I = imresize(I, 0.5));
+  end
+  for j = 1:scale
+    if(j > 2)
+      imgPyramid(i, j) = imgaussfilt(imgPyramid(i, j-1),sigma);
+    end
   end
 end
 
@@ -23,21 +27,18 @@ imgDiff = [];
 k = 2^(1/scale);
 
 for i = 1:octave
-  for j = 1:scale
+  for j = 1:(scale-1)
     # p = (sigma, sigma*k,..., sigma*k^scale -1)
-    gaussian1 = fspecial('Gaussian', 10, ((k*(j-1))*sigma));  #G(x,y,p)
-    gaussian2 = fspecial('Gaussian', 10, k*((k*(j-1))*sigma)); #G(x,y,kp)
-    dog = gaussian2 - gaussian1;
-    dogFilterImage = conv2(imgPyramid(i, j), dog, 'same');
-    imgDiff(i, j) = dogFilterImage;
+    gaussianImg1 = imgaussfilt(imgPyramid(i, j),((k*(j-1))*sigma)); #G(x,y,p)
+    gaussianImg2 = imgaussfilt(imgPyramid(i, j+1),k*((k*(j-1))*sigma)); #G(x,y,kp)
+    gaussianDiffImg = imsubtract(gaussianImg2,gaussianImg1);
+    imgDiff(i, j) = immultiply(img, gaussianDiffImg);
   end
 end
     
-## Need to compute difference of gaussians now 
- 
-## difference of gaussians
- 
-## gaussian1 = fspecial('Gaussian', 21, 15);
-## gaussian2 = fspecial('Gaussian', 21, 20);
-## dog = gaussian1 - gaussian2;
-## dogFilterImage = conv2(double(grayImage), dog, 'same');
+## find local maxima in each scale
+for i = 1:octave
+  for j = 1:(scale-1)
+    imgDiff(i, j)
+  end
+end
