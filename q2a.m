8b90cd3f7034fe7c06aafae8d47d234fb90dacce
Mansi Vaghela <mvaghela@localhost.localdomain>
2017-02-18 17:02:10 -0500

    code for question 2

---
+++ q2a.m
@@ -0,0 +1,21 @@
+addpath('./sift');
+
+refImg = imread('reference.png');
+refImg = rgb2gray(refImg);
+testImg = imread('test.png');
+testImg = rgb2gray(testImg);
+ 
+[refFrame, refDescr] = sift(im2double(refImg));
+[testFrame, testDescr] = sift(im2double(testImg));
+
+imshow(refImg);
+hold on;
+refI = plotsiftframe(refFrame(:,1:100));
+set(refI,'color','y','linewidth',1);
+hold off;
+ 
+imshow(testImg);
+hold on;
+testI = plotsiftframe(testFrame(:,1:100));
+set(testI,'color','y','linewidth',1) ;
+hold off;
